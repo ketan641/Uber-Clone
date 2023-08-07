a95175918dc957d7911ae17fcac8d0ab595d4f2b
ketan641 <ketanrehal641@gmail.com>
2023-08-07 18:46:10 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,55 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, KeyboardAvoidingView } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Platform } from "react-native";
+
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+        <KeyboardAvoidingView style={{flex: 1}}
+        behavior={Platform.OS=== "ios" ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS=== "ios" ? -64 : 0}
+        >
+        <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </KeyboardAvoidingView>
+          
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
