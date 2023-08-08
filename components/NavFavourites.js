a6288aec5c91d308a63a2b76312f2fbf34f51cef
ketan641 <ketanrehal641@gmail.com>
2023-08-08 23:26:12 +0530

    commit

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -33,6 +33,7 @@ const NavFavourites = () => {
       renderItem={({ item: { location, destination, icon } }) => (
         <TouchableOpacity style={ tw`flex-row items-center p-5`}>
           <Icon
+          
             style={tw`mr-4 rounded-full bg-gray-300 p-3`}
             name={icon}
             type="ionicon"
